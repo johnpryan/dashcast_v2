aaff89985ebfae13a1b7b93cc542030a126dc440
John Ryan <ryjohn@google.com>
2021-01-11 11:43:07 -0800

    add audio proxy handler

--- dashcast_server/lib/src/routes.dart
+++ dashcast_server/lib/src/routes.dart
@@ -4,6 +4,9 @@ import 'dart:io';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:http/http.dart' as http;
+import 'package:pedantic/pedantic.dart';
+import 'package:path/path.dart' as path;
+
 
 import '../models.dart';
 
@@ -53,19 +56,68 @@ class DashcastService {
 
   @Route.get('/podcast/<id>/episode/<episodeId>/audio')
   Future<Response> getPodcastAudio(
-      Request request, String id, String episodeId) async {
-    var sourceUrl = podcasts.firstWhere((p) => p.id == int.parse(id))
+      Request serverRequest, String id, String episodeId) async {
+    var proxyName = 'dashcast';
+    var sourceUri = Uri.parse(podcasts
+        .firstWhere((p) => p.id == int.parse(id))
         .episodes
         .firstWhere((e) => e.id == int.parse(episodeId))
-        .audioUrl;
-    // var streamedResponse = await http.Client()
-    //     .send(http.StreamedRequest('GET', Uri.parse(sourceUrl)));
-    // print('got streamedResponse $streamedResponse');
-    var response = await http.get(sourceUrl);
+        .audioUrl);
+    var requestUrl = sourceUri.resolve(serverRequest.url.toString());
+
+    var client = http.Client();
+    var clientRequest = http.StreamedRequest('GET', sourceUri);
+    clientRequest.headers.addAll(serverRequest.headers);
+    clientRequest.headers['Host'] = sourceUri.authority;
+    _addHeader(clientRequest.headers, 'via',
+        '${serverRequest.protocolVersion} $proxyName');
+
+    unawaited(serverRequest
+        .read()
+        .forEach(clientRequest.sink.add)
+        .catchError(clientRequest.sink.addError)
+        .whenComplete(clientRequest.sink.close));
+
+    var clientResponse = await client.send(clientRequest);
+    _addHeader(clientResponse.headers, 'via', '1.1 $proxyName');
+    clientResponse.headers.remove('transfer-encoding');
+    // If the original response was gzipped, it will be decoded by [client]
+    // and we'll have no way of knowing its actual content-length.
+    if (clientResponse.headers['content-encoding'] == 'gzip') {
+      clientResponse.headers.remove('content-encoding');
+      clientResponse.headers.remove('content-length');
+
+      // Add a Warning header. See
+      // http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.5.2
+      _addHeader(
+          clientResponse.headers, 'warning', '214 $proxyName "GZIP decoded"');
+    }
+
+    // Make sure the Location header is pointing to the proxy server rather
+    // than the destination server, if possible.
+    if (clientResponse.isRedirect &&
+        clientResponse.headers.containsKey('location')) {
+      var location =
+      requestUrl.resolve(clientResponse.headers['location']).toString();
+      if (path.url.isWithin(sourceUri.toString(), location)) {
+        clientResponse.headers['location'] =
+            '/' + path.url.relative(location, from: sourceUri.toString());
+      } else {
+        clientResponse.headers['location'] = location;
+      }
+    }
+
     return Response(200,
-        body: response.bodyBytes,
-        headers: Map.from(_defaultHeaders)
-          ..addAll({'Content-Type': 'Content-Type: audio/mpeg'}
-            ..addAll(response.headers)));
+        body: clientResponse.stream,
+        headers: clientResponse.headers);
   }
 }
+
+void _addHeader(Map<String, String> headers, String name, String value) {
+  if (headers.containsKey(name)) {
+    headers[name] += ', $value';
+  } else {
+    headers[name] = value;
+  }
+}
+
